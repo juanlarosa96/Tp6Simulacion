1b2fdc8ee2e62736ccb22369997e6ad5405978a7
Juan Larosa <juanlarosa96@gmail.com>
2018-05-07 17:33:28 -0300

    Agrego colas

--- TP6Simulacion/Simulacion.cs
+++ TP6Simulacion/Simulacion.cs
@@ -11,8 +11,11 @@ namespace TP6Simulacion
         Random r;
         private Random random;
         private long cantidadNucleos;
-        public List<Queue<Proceso>> colasCPU;
+        private List<Queue<Proceso>> colasCPU;
+        private Queue<Proceso> colaIO;
+        private List<Evento> eventos;
 
+        public Int32 cantidadProcesos { get; set; }
         public Int64 tiempo { get; set; }
         public Simulacion(Random r) {
             this.r = r;
@@ -21,14 +24,20 @@ namespace TP6Simulacion
         public Simulacion(Random random, long cantidadNucleos)
         {
             this.random = random;
+            this.tiempo = 0;
             List<Queue<Proceso>> colasCPU = new List<Queue<Proceso>>();
+            
             for (int i = 0; i < cantidadNucleos; i++)
             {
                 colasCPU.Add(new Queue<Proceso>());
             }
+
+            colaIO = new Queue<Proceso>();
+            eventos = new List<Evento>();
+            eventos.Add(new Llegada(generarTiempoProximaLlegada()));
         }
 
-        public Int64 generarTiempoProximaLlegada() {
+        public Int32 generarTiempoProximaLlegada() {
             //Poner fdp
             return 5;
         }

3e7faa0da86a738f4cfa789e3b9bdf1c065a0202
Juan Larosa <juanlarosa96@gmail.com>
2018-05-07 14:15:57 -0300

    Commit inicial

---
+++ TP6Simulacion/FormPrincipal.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace TP6Simulacion
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.txtNucleos = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(298, 135);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(102, 36);
+            this.btnIniciar.TabIndex = 0;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            // 
+            // txtNucleos
+            // 
+            this.txtNucleos.Location = new System.Drawing.Point(141, 135);
+            this.txtNucleos.Name = "txtNucleos";
+            this.txtNucleos.Size = new System.Drawing.Size(100, 20);
+            this.txtNucleos.TabIndex = 1;
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 422);
+            this.Controls.Add(this.txtNucleos);
+            this.Controls.Add(this.btnIniciar);
+            this.Name = "FormPrincipal";
+            this.Text = "Inicio";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.TextBox txtNucleos;
+    }
+}
---
+++ TP6Simulacion/FormSimulacion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP6Simulacion
+{
+    public partial class FormSimulacion : Form
+    {
+
+        Int64 cantidadNucleos;
+        List<Queue<Proceso>> colasCPU;
+
+        public FormSimulacion()
+        {
+            InitializeComponent();
+        }
+
+        public FormSimulacion(Int64 cantidadNucleos)
+        {
+            InitializeComponent();
+            this.cantidadNucleos = cantidadNucleos;
+        }
+
+        private void FormSimulacion_Load(object sender, EventArgs e)
+        {
+            List<Queue<Proceso>> colasCPU = new List<Queue<Proceso>>();
+            for (int i = 0; i < cantidadNucleos; i++) {
+                colasCPU.Add(new Queue<Proceso>());
+            }
+        }
+    }
+}

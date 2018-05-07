3bee56b4ca97f3b359e77e3f6cd0a9824a570529
Juan Larosa <juanlarosa96@gmail.com>
2018-05-07 16:18:59 -0300

    Agrego clase Simulacion

---
+++ TP6Simulacion/Simulacion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP6Simulacion
+{
+    public class Simulacion
+    {
+        Random r;
+        private Random random;
+        private long cantidadNucleos;
+        public List<Queue<Proceso>> colasCPU;
+
+        public Int64 tiempo { get; set; }
+        public Simulacion(Random r) {
+            this.r = r;
+        }
+
+        public Simulacion(Random random, long cantidadNucleos)
+        {
+            this.random = random;
+            List<Queue<Proceso>> colasCPU = new List<Queue<Proceso>>();
+            for (int i = 0; i < cantidadNucleos; i++)
+            {
+                colasCPU.Add(new Queue<Proceso>());
+            }
+        }
+
+        public Int64 generarTiempoProximaLlegada() {
+            //Poner fdp
+            return 5;
+        }
+
+    }
+}

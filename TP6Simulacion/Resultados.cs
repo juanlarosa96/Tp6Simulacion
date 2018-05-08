689c07c5e75ee79e54d486b82203a767b86070e3
Juan Larosa <juanlarosa96@gmail.com>
2018-05-08 18:00:30 -0300

    Agrego clase estatica Resultados que calculara los resultados

---
+++ TP6Simulacion/Resultados.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP6Simulacion
+{
+    public static class Resultados
+    {
+        public static List<Int32> iniciosTiempoOcioso { get; set; }
+        public static List<Int32> finesTiempoOcioso { get; set; }
+        public static Int32 cantidadRafagas { get; set; }
+        public static Int32 iniciosEsperas { get; set; }
+        public static Int32 finesEsperas { get; set; }
+        public static Int32 nucleos { get; set; }
+
+
+        public static void inicializar(Int32 n)
+        {
+            nucleos = n;
+            for (int i = 0; i < nucleos; i++)
+            {
+                iniciosTiempoOcioso.Add(0);
+                finesTiempoOcioso.Add(0);
+            }
+            cantidadRafagas = 0;
+            iniciosEsperas = 0;
+            finesEsperas = 0;
+        }
+
+        public static Int32 calcularTiempoOcioso()
+        {
+            Int32 acumulador = 0;
+            for (int i = 0; i < nucleos; i++)
+            {
+                acumulador = acumulador + finesTiempoOcioso[i] - iniciosTiempoOcioso[i];                
+            }
+
+            return acumulador;
+        }
+    }
+}
+
